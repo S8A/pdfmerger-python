f65ea5a1f05d90f13b7360187c35720d19bfb3d5
S8A <samuelochoap@gmail.com>
2020-05-17 10:04:06 -0400

    Add docstrings, fix merge function, fix code style

--- pdfmerger/__main__.py
+++ pdfmerger/__main__.py
@@ -1,64 +1,69 @@
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 import PyPDF2, sys
-from . import document as doc
+from .document import Document
 
 
 class DocumentTableModel(QAbstractTableModel):
-    def __init__(self, document_list, parent = None):
-        QAbstractTableModel.__init__(self, parent)
-        self.documents = document_list
+    """Internal model for the table of documents to merge."""
+
+    def __init__(self, parent = None):
+        super().__init__(parent)
+        self.docs = []
         self.headers = ['File', 'Start', 'End']
 
     def rowCount(self, parent):
-        return len(self.documents)
+        """Returns the number of documents."""
+        return len(self.docs)
 
     def columnCount(self, parent):
+        """Returns the number of columns."""
         return len(self.headers)
 
     def flags(self, index):
-        row = index.row()
-        col = index.column()
-        if col == 0:
+        """Provides the properties (flags) of each column."""
+        if index.column() == 0:
             return Qt.ItemIsEnabled | Qt.ItemIsSelectable
         else:
             return Qt.ItemIsEditable | Qt.ItemIsEnabled | Qt.ItemIsSelectable
 
     def data(self, index, role):
+        """Provides the data for each cell."""
         row = index.row()
         col = index.column()
 
         if role == Qt.EditRole:
             if col == 1:
-                return self.documents[row].start
+                return self.docs[row].start
             elif col == 2:
-                return self.documents[row].end
+                return self.docs[row].end
 
         if role == Qt.DisplayRole:
             if col == 0:
-                return self.documents[row].path
+                return self.docs[row].path
             elif col == 1:
-                return self.documents[row].start
+                return self.docs[row].start
             elif col == 2:
-                return self.documents[row].end
+                return self.docs[row].end
 
     def setData(self, index, value, role = Qt.EditRole):
+        """Handles the modification of editable cells."""
         if role == Qt.EditRole:
             row = index.row()
             col = index.column()
-            max_value = self.documents[row].pdf.numPages
-            old_start = self.documents[row].start
-            old_end = self.documents[row].end
+            total_pages = self.docs[row].num_pages
+            old_start = self.docs[row].start
+            old_end = self.docs[row].end
 
             try:
                 new_value = int(value)
                 if col == 1 and new_value > 0 and new_value <= old_end:
-                    self.documents[row].start = new_value
+                    self.docs[row].start = new_value
                     self.dataChanged.emit(index, index)
                     return True
                 elif (col == 2 and new_value >= old_start
-                        and new_value <= max_value):
-                    self.documents[row].end = new_value
+                        and new_value <= total_pages):
+                    self.docs[row].end = new_value
                     self.dataChanged.emit(index, index)
                     return True
             except TypeError:
@@ -66,40 +71,41 @@ class DocumentTableModel(QAbstractTableModel):
         return False
 
     def headerData(self, section, orientation, role):
+        """Returns the appropriate header."""
         if role == Qt.DisplayRole:
             if orientation == Qt.Horizontal:
                 if section < len(self.headers):
                     return self.headers[section]
-                else:
-                    return "Not implemented"
 
     def insertRows(self, row, count, parent = QModelIndex()):
+        """Handles the insertion of document entries into the list."""
         self.beginInsertRows(parent, row, row + count - 1)
         for i in range(count):
-            self.documents.insert(row, doc.Document())
-        self.endInsertRows()
-        
+            self.docs.insert(row, Document())
+        self.endInsertRows()        
         return True
 
     def removeRows(self, row, count, parent = QModelIndex()):
+        """Handles the removal of document entries from the list."""
         self.beginRemoveRows(parent, row, row + count - 1);
         for i in range(count):
             self.documents.pop(row)
         self.endRemoveRows()
-
         return True
 
 
 class PDFMergerWindow(QMainWindow):
+    """Main window of the PDF Merger application."""
+
     def __init__(self):
         super().__init__()
-        self.initUI()
+        self._create_ui()
 
-    def initUI(self):
-        self.docs = []
-        self.model = DocumentTableModel(self.docs)
+    def _create_ui(self):
+        """Creates the window's UI."""
+        self.model = DocumentTableModel()
 
-        self.table = QTableView(self)
+        self.table = QTableView()
         self.table.setModel(self.model)
         self.table.setSelectionMode(
             QAbstractItemView.SelectionMode.SingleSelection)
@@ -110,35 +116,30 @@ class PDFMergerWindow(QMainWindow):
         header.setSectionResizeMode(2, QHeaderView.ResizeToContents)
 
         self.selection = self.table.selectionModel()
-        self.selection.currentChanged.connect(self.toggle_buttons)
+        self.selection.currentChanged.connect(self._toggle_buttons)
 
         self.setCentralWidget(self.table)
 
-        self.add_action = QAction('Add', self)
-        self.add_action.triggered.connect(self.add_file)
+        self.add_action = QAction('Add')
+        self.add_action.triggered.connect(self._add_file)
 
-        self.clear_action = QAction('Clear', self)
-        self.clear_action.triggered.connect(self.remove_all)
+        self.clear_action = QAction('Clear')
+        self.clear_action.triggered.connect(self._clear)
 
-        self.duplicate_action = QAction('Duplicate', self)
-        self.duplicate_action.setEnabled(False)
-        self.duplicate_action.triggered.connect(self.duplicate)
+        self.duplicate_action = QAction('Duplicate')
+        self.duplicate_action.triggered.connect(self._duplicate)
 
-        self.remove_action = QAction('Remove', self)
-        self.remove_action.setEnabled(False)
-        self.remove_action.triggered.connect(self.remove)
+        self.remove_action = QAction('Remove')
+        self.remove_action.triggered.connect(self._remove)
 
-        self.move_up_action = QAction('Move Up', self)
-        self.move_up_action.setEnabled(False)
-        self.move_up_action.triggered.connect(self.move_up)
+        self.move_up_action = QAction('Move Up')
+        self.move_up_action.triggered.connect(self._move_up)
 
-        self.move_down_action = QAction('Move Down', self)
-        self.move_down_action.setEnabled(False)
-        self.move_down_action.triggered.connect(self.move_down)
+        self.move_down_action = QAction('Move Down')
+        self.move_down_action.triggered.connect(self._move_down)
 
-        self.merge_action = QAction('Merge', self)
-        self.merge_action.setEnabled(False)
-        self.merge_action.triggered.connect(self.merge)
+        self.merge_action = QAction('Merge')
+        self.merge_action.triggered.connect(self._merge)
 
         toolbar = self.addToolBar('Main')
         toolbar.setMovable(False)
@@ -154,12 +155,19 @@ class PDFMergerWindow(QMainWindow):
         toolbar.addSeparator()
         toolbar.addAction(self.merge_action)
 
-    def change_selection(self, index):
+        self._toggle_buttons()
+
+        self.setWindowTitle("PDFMerger")
+        self.resize(600, 400)
+
+    def _change_selection(self, index):
+        """Changes the selected item to the one with the given index."""
         self.selection.setCurrentIndex(
             self.selection.currentIndex().siblingAtRow(index),
             QItemSelectionModel.SelectionFlag.ClearAndSelect)
 
-    def toggle_buttons(self):
+    def _toggle_buttons(self):
+        """Enables or disables buttons according to selected row."""
         current = self.selection.currentIndex().row()
 
         self.duplicate_action.setEnabled(False)
@@ -170,92 +178,100 @@ class PDFMergerWindow(QMainWindow):
         if current != -1:
             self.duplicate_action.setEnabled(True)
             self.remove_action.setEnabled(True)
-
             if current != 0:
                 self.move_up_action.setEnabled(True)
-            if current != len(self.docs) - 1:
+            if current != len(self.model.docs) - 1:
                 self.move_down_action.setEnabled(True)
 
-        if len(self.docs) > 0:
+        if len(self.model.docs) > 0:
             self.merge_action.setEnabled(True)
         else:
             self.merge_action.setEnabled(False)
 
-    def add_file(self):
+    def _add_file(self):
+        """Adds a PDF file to the end of the list."""
         file_dialog = QFileDialog()
         file_dialog.setFileMode(QFileDialog.FileMode.ExistingFiles)
         to_open = file_dialog.getOpenFileNames(
-                self, caption = 'Open file', filter = '*.pdf')
+            self, caption = 'Open file', filter = '*.pdf')
 
         for file in to_open[0]:
-            pdf = doc.Document(file)
+            doc = Document(file)
             self.model.layoutAboutToBeChanged.emit()
-            self.docs.append(pdf)
+            self.model.docs.append(doc)
             self.model.layoutChanged.emit()
 
-    def remove(self):
+    def _remove(self):
+        """Removes the current PDF entry from the list."""
         current = self.selection.currentIndex().row()
         self.model.layoutAboutToBeChanged.emit()
-        self.docs.pop(current)
+        self.model.docs.pop(current)
         self.model.layoutChanged.emit()
-        self.selection.clear()
+        if self.model.docs:
+            self._change_selection(current - 1)
+        else:
+            self.selection.clear()
 
-    def remove_all(self):
+    def _clear(self):
+        """Removes all entries from the PDF list."""
         self.model.layoutAboutToBeChanged.emit()
-        self.docs.clear()
+        self.model.docs.clear()
         self.model.layoutChanged.emit()
         self.selection.clear()
 
-    def duplicate(self):
+    def _duplicate(self):
+        """Creates a copy of the current PDF entry."""
         current = self.selection.currentIndex().row()
         self.model.insertRows(current + 1, 1)
-        copy = doc.Document(self.docs[current].path)
-        copy.set_interval(self.docs[current].start, self.docs[current].end)
-        self.docs[current + 1] = copy
+        self.model.docs[current + 1] = self.model.docs[current].copy()
 
-    def swap(self, i, j):
+    def _swap(self, i, j):
+        """Swaps two PDF entries' positions in the list."""
         self.model.layoutAboutToBeChanged.emit()
-        self.docs[i], self.docs[j] = self.docs[j], self.docs[i]
+        self.model.docs[i], self.model.docs[j] = (
+            self.model.docs[j], self.model.docs[i])
         self.model.layoutChanged.emit()
 
-    def move_up(self):
+    def _move_up(self):
+        """Moves the current PDF entry up one position."""
         current = self.selection.currentIndex().row()
-        self.swap(current - 1, current)
-        self.change_selection(current - 1)
+        self._swap(current - 1, current)
+        self._change_selection(current - 1)
 
-    def move_down(self):
+    def _move_down(self):
+        """Moves the current PDF entry down one position."""
         current = self.selection.currentIndex().row()
-        self.swap(current, current + 1)
-        self.change_selection(current + 1)
+        self._swap(current, current + 1)
+        self._change_selection(current + 1)
 
-    def merge(self):
-        if len(self.docs) > 0:
-            writer = PyPDF2.PdfFileWriter()
+    def _merge(self):
+        """Merges the PDF entries in the list into one PDF and saves it."""
+        if len(self.model.docs) > 0:
+            merger = PyPDF2.PdfFileMerger()
 
-            for document in self.docs:
-                start = document.start - 1
-                end = document.end
-                for i in range(start, end):
-                    page = document.pdf.getPage(i)
-                    writer.addPage(page)
+            for doc in self.model.docs:
+                pdf = PyPDF2.PdfFileReader(open(doc.path, 'rb'))
+                page_range = (doc.start - 1, doc.end)
+                merger.append(pdf, pages=page_range)
 
             file_dialog = QFileDialog()
             file_dialog.setFileMode(QFileDialog.FileMode.AnyFile)
             file_dialog.setAcceptMode(QFileDialog.AcceptMode.AcceptSave)
             to_save = file_dialog.getSaveFileName(
-                    self, caption = 'Save file', filter = '*.pdf')
+                self, caption = 'Save file', filter = '*.pdf')
 
             if to_save[0]:
-                output = open(to_save[0], 'wb')
-                writer.write(output)
-                output.close()
+                with open(to_save[0], 'wb') as file:
+                    merger.write(file)
 
 
-if __name__ == '__main__':
+def main():
+    """Creates and shows the main window of the application."""
     app = QApplication(sys.argv)
     window = PDFMergerWindow()
-    window.setWindowTitle("PDFMerger")
-    window.resize(600, 400)
     window.show()
     sys.exit(app.exec())
 
+
+if __name__ == '__main__':
+    main()
--- pdfmerger/document.py
+++ pdfmerger/document.py
@@ -2,20 +2,30 @@ import PyPDF2
         
 
 class Document():
-    """Represents a PDF document entry."""
+    """Representation of a PDF document entry."""
+
     def __init__(self, file = None):
         super().__init__()
         self.path = ""
         self.start = 1
         self.end = 1
+        self.num_pages = 1
         if file is not None:
             self.path = file
-            self.pdf = PyPDF2.PdfFileReader(open(file, 'rb'))
-            self.end = self.pdf.numPages
+            pdf = PyPDF2.PdfFileReader(open(file, 'rb'))
+            self.num_pages = pdf.numPages
+            self.end = pdf.numPages
 
     def __repr__(self):
         return f"{self.__class__.__name__}({self.path})"
 
     def set_interval(self, start, end):
+        """Sets the start and end page of the document."""
         self.start = start
         self.end = end
+    
+    def copy(self):
+        """Creates a copy of the document."""
+        copy = Document(self.path)
+        copy.set_interval(self.start, self.end)
+        return copy
